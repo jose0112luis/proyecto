38b01805798f6373225f47e0bb262dd830607f3d
jose0112luis <jose0112luis@hotmail.com>
2023-12-13 18:08:58 -0500

    Creación de DTOs para cada Entidad

---
+++ src/main/java/org/proyecto/dto/ProcesoDto.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.proyecto.dto;
+
+/**
+ *
+ * @author jose0
+ */
+public class ProcesoDto {
+    
+    private Integer id;
+    private String descripcion;
+
+    public ProcesoDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
